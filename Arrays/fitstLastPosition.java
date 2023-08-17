b436976da3761b0adce48c04f2e0508ad97fe686
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2023-08-17 23:32:30 +0530

    Add files via upload

---
+++ Arrays/fitstLastPosition.java
@@ -0,0 +1,22 @@
+package Github;
+
+public class fitstLastPosition {
+    public static int[] searchRange(int[] nums, int target) {
+        int arr[] = new int[2];
+        arr[0] = arr[1] = -1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                arr[0] = i;
+                while (i < nums.length && nums[i] == target) {
+                    i++;
+                }
+                arr[1] = i - 1;
+
+                break;
+            }
+        }
+
+        return arr;
+    }
+}
