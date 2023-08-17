914346a7efa28828d1de4335254c29add26f3173
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2023-08-18 00:49:00 +0530

    Add files via upload

---
+++ Arrays/arrayTripletPairSum.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class arrayTripletPairSum {
+    public static void main(String[] args) {
+        int arr[] = {10, 20, 30, 40, 50, 60, 70};
+        int sum=180;
+        for(int i=0 ; i<arr.length; i++){
+            for (int j=i+1; j<arr.length; j++) {
+                for (int k = j + 1; k < arr.length; k++) {
+                    if (sum == (arr[i] + arr[j] + arr[k])) {
+                        System.out.println("(" + arr[i] + "," + arr[j] + ","+arr[k] +")");
+                    }
+                }
+            }
+        }
+    }
+}
