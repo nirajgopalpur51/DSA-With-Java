2b41121d07e675954bde6193a3de2f5ed8966928
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2023-08-18 11:13:47 +0530

    Add files via upload

---
+++ Arrays/copyCreation.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class copyCreation {
+
+    public static void inc(int a){
+        a++;
+        System.out.println(a);
+    }
+
+    public static void main(String[] args) {
+        int a=5;
+        inc(a);
+        System.out.println(a);
+    }
+}
