869909bccb367c582d09b641d0089d879bfae77f
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2023-08-19 18:01:07 +0530

    Add files via upload

---
+++ Arrays/uniqueElement.java
@@ -0,0 +1,25 @@
+package Array;
+
+import java.util.Scanner;
+
+public class uniqueElement {
+
+    public static int unique(int arr[]){
+        int ans=0;
+        for(int i=0; i<arr.length; i++){
+            ans=ans^arr[i];
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("ENter the size of the array");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+
+        System.out.println(unique(arr));
+    }
+}
