061c14f7fda820fa4d4522734e5546667d53dba6
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2023-08-19 18:00:45 +0530

    Add files via upload

---
+++ Arrays/zero_Ones.java
@@ -0,0 +1,26 @@
+package Array;
+
+public class zero_Ones {
+    public static void main(String[] args) {
+        int arr[]={0,1,1,0,1,0,1,1,1,0,1,1};
+        int start=0;
+        int end=arr.length-1;
+        int i=0;
+        while(i!=end){
+            if(arr[i]==0){
+                arr[i]=arr[start];
+                start++;
+                i++;
+            }
+            else{
+                int temp=arr[i];
+                arr[i]=arr[end];
+                arr[end]=temp;
+                end--;
+            }
+        }
+        for (int e:arr) {
+            System.out.print(e+" ");
+        }
+    }
+}
