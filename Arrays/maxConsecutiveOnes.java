7f0bd7937c38bd61f97eee13162da7e0e1dcfb1e
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2023-08-19 19:06:48 +0530

    Add files via upload

---
+++ Arrays/maxConsecutiveOnes.java
@@ -0,0 +1,24 @@
+package Github;
+
+public class maxConsecutiveOnes {
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int count=0;
+        int maxCount=0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]==1){
+                count++;
+                maxCount=Math.max(maxCount,count);
+            }
+            else{
+                count=0;
+            }
+        }
+
+        return maxCount;
+    }
+
+    public static void main(String[] args) {
+        int arr[]={1,1,0,1,1,1};
+        System.out.println(findMaxConsecutiveOnes(arr));
+    }
+}
