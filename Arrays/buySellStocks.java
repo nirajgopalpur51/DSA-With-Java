1eff968761cf3fbab97145ac5c26617cb3b8623a
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2023-08-20 23:08:53 +0530

    Add files via upload

---
+++ Arrays/buySellStocks.java
@@ -0,0 +1,22 @@
+package Github;
+
+public class buySellStocks {
+    public static int maxProfit(int[] prices) {
+        int buyPrice=Integer.MAX_VALUE;
+        int maxProfit=0;
+        for(int i=0; i<prices.length; i++){
+            if(buyPrice<prices[i]){
+                int profit=prices[i]-buyPrice;
+                maxProfit=Math.max(maxProfit,profit);
+            }
+            else{
+                buyPrice=prices[i];
+            }
+        }
+        return maxProfit;
+    }
+    public static void main(String[] args) {
+        int prices[]={7,1,5,3,6,4};
+        System.out.println(maxProfit(prices));
+    }
+}
