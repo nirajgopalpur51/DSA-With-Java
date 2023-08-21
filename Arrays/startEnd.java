82f6669200803de64a5823596fcd141bc9cbed2f
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2023-08-21 19:09:22 +0530

    Add files via upload

---
+++ Arrays/startEnd.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class startEnd {
+    public static void main(String[] args) {
+
+        int arr[] = {10, 20, 30, 70,40, 50,60};
+        int n=arr.length-1;
+        for(int i=0; i<=n; i++){
+//            if(i>n){
+//                break;
+//            }
+            if(i==n){
+                System.out.println(arr[i]);
+            }
+            else{
+                System.out.println(arr[i]);
+                System.out.println(arr[n]);
+            }
+
+            n--;
+        }
+    }
+}
