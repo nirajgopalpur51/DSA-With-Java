4a42e553c77f86e129552022a0675c191c144ac0
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2023-08-22 23:30:50 +0530

    Add files via upload

---
+++ Arrays/sortColores.java
@@ -0,0 +1,26 @@
+package Array;
+
+public class sortColores {
+    public static void main(String[] args) {
+//        public void sortColors(int number[]) {
+//            int l=0; int m=0; int h=number.length-1;
+//            while(m<=h){
+//                if(number[m]==0){
+//                    int temp=number[l];
+//                    number[l]=number[m];
+//                    number[m]=temp;
+//                    l++;m++;
+//                }
+//                else if(number[m]==1){
+//                    m++;
+//                }
+//                else{
+//                    int temp=number[m];
+//                    number[m]=number[h];
+//                    number[h]=temp;
+//                    h--;
+//                }
+//            }
+//        }
+    }
+}
