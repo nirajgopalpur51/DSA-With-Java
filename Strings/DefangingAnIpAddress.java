1db996fdf470c518de130aa816d1cc9957ac48aa
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2023-08-30 18:26:09 +0530

    Add files via upload

---
+++ Strings/DefangingAnIpAddress.java
@@ -0,0 +1,11 @@
+package Github;
+
+public class DefangingAnIpAddress {
+    public static String defangIPaddr(String address) {
+        return address.replace(".","[.]");
+    }
+    public static void main(String[] args) {
+        String address="1.1.1.1";
+        System.out.println(defangIPaddr(address));
+    }
+}
