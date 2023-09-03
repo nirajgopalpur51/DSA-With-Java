4a8488790966f73c0c4406715059c43850af39ad
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2023-09-03 11:41:20 +0530

    Add files via upload

---
+++ Arrays/pascalTriangle.java
@@ -0,0 +1,25 @@
+package Github;
+import java.util.*;
+public class pascalTriangle {
+    public static List<List<Integer>> generate(int numRows) {
+        if(numRows==0) return new  ArrayList();
+        List<List<Integer>> result=new ArrayList();
+        for(int i=1; i<=numRows; i++){
+            List<Integer> row=new ArrayList();
+            for(int j=0; j<i; j++){
+                if(j==0 || j==i-1){
+                    row.add(1);
+                }
+                else{
+                    row.add(result.get(i-2).get(j)+result.get(i-2).get(j-1));
+                }
+            }
+            result.add(row);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(generate(5));
+    }
+}
