62bada2b32605dffb6d9d36e3836ef5912c86959
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2023-09-06 19:09:44 +0530

    Add files via upload

---
+++ Strings/rotateString.java
@@ -0,0 +1,12 @@
+package Github;
+
+public class rotateString {
+    public static boolean rotateString(String s, String goal) {
+        return (s.length()==goal.length() && (s+s).contains(goal));
+    }
+
+    public static void main(String[] args) {
+        boolean b=rotateString("abcde","cdeab");
+        System.out.println(b);
+    }
+}
