ccbe15f90212c92e2a3290676a79b023af09db08
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2023-09-07 17:35:16 +0530

    Add files via upload

---
+++ Strings/RansomNote.java
@@ -0,0 +1,22 @@
+package Github;
+
+public class RansomNote {
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        int count[]=new int[26];
+
+        for(int ch:magazine.toCharArray()){
+            count[ch-'a']++;   // 0 to 1
+        }
+        for(int ch:ransomNote.toCharArray()){
+            count[ch-'a']--;
+            if(count[ch-'a']<0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        boolean b=canConstruct("aa","aab");
+        System.out.println(b);
+    }
+}
