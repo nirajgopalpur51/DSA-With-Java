51184cf99e3dbe5cec366b5227c518d958d7dae6
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2023-09-08 08:10:22 +0530

    Add files via upload

---
+++ Strings/ReverseStringII.java
@@ -0,0 +1,31 @@
+package Github;
+
+public class ReverseStringII {
+    public static String reverseStr(String s, int k) {
+        char[] str = s.toCharArray();
+        int n = str.length;
+        for(int i = 0 ; i <= n-1; i += 2*k){
+            if(i+k-1 <= n-1){
+                reverseK(i,i+k-1,str);
+            }else{
+                //for fewer than k characters left (edge case)
+                reverseK(i,n-1,str);
+            }
+        }
+        String ans = new String(str);
+        return ans;
+    }
+    public static void reverseK(int i, int j, char[] str){
+        while(i < j){
+            char temp = str[i];
+            str[i] = str[j];
+            str[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseStr("abcdefg", 2));
+    }
+}
