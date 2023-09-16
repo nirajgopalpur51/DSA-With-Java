1f7cbf844091bf331b71c6cea932b1f200309b13
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2023-09-16 18:58:02 +0530

    Add files via upload

---
+++ PalindromeNumber.java
@@ -0,0 +1,19 @@
+package Github;
+
+public class PalindromeNumber {
+    public static boolean isPalindrome(int x) {
+        String number = Integer.toString(x);
+        int length = number.length();
+        for (int i = 0; i < length / 2; i++) {
+            if (number.charAt(i) != number.charAt(length - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        boolean b=isPalindrome(121);
+        System.out.println(b);
+    }
+}
