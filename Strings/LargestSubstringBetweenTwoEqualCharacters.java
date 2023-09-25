f93ede35977f21299b439ac0d3228c1264a92c8f
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2023-09-26 00:04:05 +0530

    Add files via upload

---
+++ Strings/LargestSubstringBetweenTwoEqualCharacters.java
@@ -0,0 +1,19 @@
+package Github;
+
+public class LargestSubstringBetweenTwoEqualCharacters {
+    public static int maxLengthBetweenEqualCharacters(String s) {
+        int max=-1;
+        for(int i=0;i<s.length();i++){
+            for(int j=s.length()-1;j>i;j--){
+                if(s.charAt(i)==s.charAt(j)){
+                    max=Math.max(max,((j-i)-1));
+                }
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxLengthBetweenEqualCharacters("abca"));
+    }
+}
