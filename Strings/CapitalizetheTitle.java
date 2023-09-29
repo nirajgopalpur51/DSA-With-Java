2cd003e32ddb8213fdf558417335f28bc397071e
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2023-09-29 15:59:46 +0530

    Add files via upload

---
+++ Strings/CapitalizetheTitle.java
@@ -0,0 +1,17 @@
+package Github;
+
+public class CapitalizetheTitle {
+    public static String capitalizeTitle(String title) {
+        StringBuilder sb = new StringBuilder();
+        for(String s : title.toLowerCase().split(" ")){
+            if(s.length() > 2)
+                sb.append(Character.toUpperCase(s.charAt(0)) + s.substring(1) + " ");
+            else
+                sb.append( s+ " ");
+        }
+        return sb.toString().trim();
+    }
+    public static void main(String[] args) {
+        System.out.println(capitalizeTitle("capiTalIze tHe titLe"));
+    }
+}
