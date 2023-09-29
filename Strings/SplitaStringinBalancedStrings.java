30dbcfaf3588317bc0183fd8d0633740c951aa76
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2023-09-29 10:14:08 +0530

    Add files via upload

---
+++ Strings/SplitaStringinBalancedStrings.java
@@ -0,0 +1,22 @@
+package Github;
+
+public class SplitaStringinBalancedStrings {
+    public static int balancedStringSplit(String s) {
+        int l=0, r=0, count =0;
+        for(Character i : s.toCharArray()){
+            if(i=='R')
+                r++;
+            else
+                l++;
+            if(l==r){
+                count++;
+                l=r=0;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(balancedStringSplit("RLRRLLRLRL"));
+    }
+}
