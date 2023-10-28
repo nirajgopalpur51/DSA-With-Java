375e0c20e1735d6dc32edfd12541fd6b00f677e4
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2023-10-28 11:39:17 +0530

    Add files via upload

---
+++ LinkedList/printLL_Recusrsive.java
@@ -0,0 +1,49 @@
+package LinkedList;
+
+import java.util.Scanner;
+
+public class printLL_Recusrsive {
+
+    public static Node<Integer> takeInput()
+    {
+        Node<Integer> head = null, tail = null;
+        Scanner s = new Scanner(System.in);
+        int data = s.nextInt();
+
+        while(data != -1){
+            Node<Integer> newNode = new Node<Integer>(data);
+            if(head == null){
+                head = newNode;
+                tail = newNode;
+            }else{
+                tail.next = newNode;
+                tail = newNode; // tail = tail.next
+            }
+            data = s.nextInt();
+        }
+        return head;
+    }
+
+    public static void print(Node<Integer> head){
+        while(head != null){
+            System.out.print(head.data +" ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void printR(Node<Integer> head){
+        if(head==null){
+            return ;
+        }
+        System.out.print(head.data+" ");
+        printR(head.next);
+    }
+
+    public static void main(String[] args) {
+        Node<Integer> head1=takeInput();
+        printR(head1);
+
+    }
+
+}
