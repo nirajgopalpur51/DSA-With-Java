82d892e8c350f46960012a89f3ec7fcfb9c321f1
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2023-10-30 10:00:19 +0530

    Add files via upload

---
+++ LinkedList/addFirst.java
@@ -0,0 +1,34 @@
+package LinkedList;
+
+public class addFirst {
+    public static Node<Integer> head ;
+    public static Node<Integer> tail;
+    public static void add(int data){
+
+
+        Node<Integer> newNode=new Node<>(data);
+        if(newNode==null){
+            head=tail=newNode;
+            return;
+        }
+
+        newNode.next=head;
+        head=newNode;
+    }
+
+    public static void print(Node<Integer> head){
+        while(head != null){
+            System.out.print(head.data +" ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        add(12);
+        add(12);
+        add(120);
+        print(head);
+
+    }
+}
