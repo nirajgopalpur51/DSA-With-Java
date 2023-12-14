b1942d43268497ca241eb794947dbf9bf0ea6b49
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2023-12-14 18:26:18 +0530

    Add files via upload

---
+++ LinkedList/ConvertBinaryLinkedListtoInteger.java
@@ -0,0 +1,21 @@
+package LinkedList;
+
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+
+public class ConvertBinaryLinkedListtoInteger {
+    public int getDecimalValue(ListNode head) {
+        int ans = 0;
+        while(head != null){
+            ans = ans * 2 + head.val;
+            head = head.next;
+        }
+        return ans;
+    }
+}
