09ad2b62e209360057bbef2cf8d9c7257486f25f
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2023-12-14 19:28:34 +0530

    Add files via upload

---
+++ LinkedList/InsertGCD.java
@@ -0,0 +1,24 @@
+package LinkedList;
+
+public class InsertGCD {
+    static int gcd(int a, int b){
+        if(b==0) return a;
+        else return gcd(b,a%b);
+    }
+    public ListNode insertGreatestCommonDivisors(ListNode head) {
+
+        if(head==null || head.next==null) return head;
+        ListNode t = head;
+
+
+        while(t.next!=null){
+            ListNode x = t.next;
+            int a = gcd(t.val, x.val);
+            t.next = new ListNode(a);
+            t=t.next;
+            t.next = x;
+            t=t.next;
+        }
+        return head;
+    }
+}
