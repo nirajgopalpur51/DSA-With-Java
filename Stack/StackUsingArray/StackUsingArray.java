c87642e2b02ec8ffd13478a154a734115025068c
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2023-12-20 18:26:46 +0530

    Add files via upload

---
+++ Stack/StackUsingArray/StackUsingArray.java
@@ -0,0 +1,55 @@
+package Stack;
+
+public class StackUsingArray {
+    private int data[];
+    private int top;
+
+    public StackUsingArray(){
+        data=new int[10];
+        top= -1;
+    }
+
+    public StackUsingArray(int capacity){
+        data = new int[capacity];
+        top = -1;
+    }
+
+    public boolean isEmpty(){
+        return (top == -1) ;
+    }
+
+    public int size(){
+        return top+1;
+    }
+
+    public int top() throws StackEmptyException {
+        if(size() == 0){
+            // StackEmptyException
+            StackEmptyException e =new StackEmptyException();
+            throw e;
+        }
+        return data[top];
+    }
+
+    public void push(int elem) throws StackFullException {
+        if(size() == data.length){
+            //StackFullException
+            StackFullException e=new StackFullException();
+            throw e;
+        }
+        top++;
+        data[top]=elem;
+    }
+
+    public int pop() throws StackEmptyException {
+        if(size() == 0){
+            // StackEmptyException
+            StackEmptyException e =new StackEmptyException();
+            throw e;
+        }
+        int temp=data[top];
+        top--;
+        return temp;
+    }
+
+}
