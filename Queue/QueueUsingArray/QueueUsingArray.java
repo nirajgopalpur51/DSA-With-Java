da57f7fd3ab1dc6877387c09811c163885cda585
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2023-12-23 23:19:46 +0530

    Update QueueUsingArray.java

--- Queue/QueueUsingArray/QueueUsingArray.java
+++ Queue/QueueUsingArray/QueueUsingArray.java
@@ -1,12 +1,12 @@
 package Queue;
 
 public class QueueUsingArray {
-	
+
 	private int data[];
 	private int front; // index at which front element is stored
 	private int rear; // index at which rear element is stored
 	private int size;
-	
+
 	public QueueUsingArray() {
 		data = new int[10];
 		front = -1;
@@ -23,7 +23,7 @@ public class QueueUsingArray {
 	int size(){
 		return size;
 	}
-	
+
 	boolean isEmpty(){
 		return size == 0;
 	}
@@ -34,13 +34,14 @@ public class QueueUsingArray {
 		}
 		return data[front];
 	}
-	
+
 	void enqueue(int element) throws QueueFullException{
-		
+
 		if(size == data.length){
-			throw new QueueFullException();
+//			throw new QueueFullException();
+			doubleCapacity();
 		}
-		
+
 		if(size == 0){
 			front = 0;
 		}
@@ -50,9 +51,26 @@ public class QueueUsingArray {
 //		if(rear == data.length){
 //			rear = 0;
 //		}
-		data[rear] = element; 
+		data[rear] = element;
 	}
-	
+
+	private void doubleCapacity() {
+		int temp[] = data;
+		data = new int[ 2* temp.length];
+		int index = 0;
+		for(int i = front ; i < temp.length; i++){
+			data[index] = temp[i];
+			index++;
+		}
+		for(int i = 0; i <= front - 1;i++){
+			data[index] = temp[i];
+			index++;
+		}
+		front = 0;
+		rear = temp.length - 1;
+	}
+
+
 	int dequeue() throws QueueEmptyException{
 		if(size == 0){
 			throw new QueueEmptyException();
@@ -70,5 +88,4 @@ public class QueueUsingArray {
 		}
 		return temp;
 	}
-	
 }
