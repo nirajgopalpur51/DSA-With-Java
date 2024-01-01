4034a0a6a8ef99644658464d355288ffadbfd43b
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2024-01-02 00:34:40 +0530

    Add files via upload

---
+++ Stack/RemovingStarsFromaString.java
@@ -0,0 +1,30 @@
+package Stack;
+
+import java.util.*;
+
+public class RemovingStarsFromaString {
+        public static String removeStars(String s) {
+            Deque<Character> stack = new ArrayDeque<>();
+
+            for (char c : s.toCharArray()) {
+                if (c == '*' && !stack.isEmpty()) {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
+            }
+
+            char[] result = new char[stack.size()];
+            int index = 0;
+            while (!stack.isEmpty()) {
+                result[index++] = stack.pop();
+            }
+
+            return new String(result);
+        }
+
+    public static void main(String[] args) {
+       String s = "leet**cod*e";
+        System.out.println(removeStars(s));
+    }
+}
