30c4886602a0073feb2093ba115939fab9d5da5a
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2024-01-05 15:29:06 +0530

    Add files via upload

---
+++ BinaryTree/LargestDataNode.java
@@ -0,0 +1,58 @@
+package BinaryTree;
+
+import java.util.Scanner;
+
+public class LargestDataNode {
+
+    public static BinaryTreeNode<Integer> takeInputLevelWise() {
+        Scanner s = new Scanner(System.in);
+        QueueUsingLL<BinaryTreeNode<Integer>> pendingNodes = new QueueUsingLL<>();
+        System.out.println("Enter root data");
+        int rootData = s.nextInt();
+        if (rootData == -1) {
+            return null;
+        }
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootData);
+        pendingNodes.enqueue(root);
+
+        while (!pendingNodes.isEmpty()) {
+            BinaryTreeNode<Integer> front;
+            try {
+                front = pendingNodes.dequeue();
+            } catch (QueueEmptyException e) {
+                return null;
+            }
+            System.out.println("Enter left child of " + front.data);
+            int leftChild = s.nextInt();
+            if (leftChild != -1) {
+                BinaryTreeNode<Integer> child = new BinaryTreeNode<Integer>(leftChild);
+                pendingNodes.enqueue(child);
+                front.left = child;
+            }
+
+            System.out.println("Enter right child of " + front.data);
+            int rightChild = s.nextInt();
+            if (rightChild != -1) {
+                BinaryTreeNode<Integer> child = new BinaryTreeNode<Integer>(rightChild);
+                pendingNodes.enqueue(child);
+                front.right = child;
+            }
+        }
+        return root;
+    }
+    public static int largest(BinaryTreeNode<Integer> root){
+        if(root == null){
+            return -1;
+        }
+
+        int leftData = largest(root.left);
+        int rightData = largest(root.right);
+
+        return Math.max(root.data,Math.max(leftData,rightData));
+    }
+
+    public static void main(String[] args) {
+        BinaryTreeNode<Integer> root = takeInputLevelWise();
+        System.out.println(largest(root));
+    }
+}
