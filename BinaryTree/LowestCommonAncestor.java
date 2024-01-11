032e058f07012b18367469e7991a62fde78f4d21
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2024-01-11 20:28:42 +0530

    Add files via upload

---
+++ BinaryTree/LowestCommonAncestor.java
@@ -0,0 +1,75 @@
+package BinaryTree;
+
+import java.util.*;
+
+public class LowestCommonAncestor {
+
+
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+
+    public static boolean getPath(Node root, int n, ArrayList<Node> path) {
+        if (root == null) {
+            return false;
+        }
+
+        path.add(root);
+
+        if (root.data == n) {
+            return true;
+        }
+
+        boolean foundLeft = getPath(root.left, n, path);
+        boolean foundRight = getPath(root.right, n, path);
+
+        if (foundLeft || foundRight) {
+            return true;
+        }
+
+        path.remove(path.size() - 1);
+        return false;
+    }
+
+    public static Node lowestCommonAncestor(Node root, Node n1, Node n2) {
+        ArrayList<Node> path1 = new ArrayList<>();
+        ArrayList<Node> path2 = new ArrayList<>();
+
+        getPath(root, n1.data, path1);
+        getPath(root, n2.data, path2);
+
+        int i = 0;
+        for (; i < path1.size() && i < path2.size(); i++) {
+            if (path1.get(i) != path2.get(i)) {
+                break;
+            }
+        }
+
+        Node lca = path1.get(i - 1);
+        return lca;
+    }
+
+    public static void main(String[] args) {
+        Node root =new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.right.right = new Node(6);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        Node subroot1 =new Node(4);
+        Node subroot2 =new Node(5);
+//        subroot.left=new Node(4);
+//        subroot.right=new Node(5);
+
+        System.out.println(lowestCommonAncestor(root,subroot1,subroot2).data);
+    }
+}
