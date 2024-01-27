100af1edf736094e6ee594485a747b63ef70428a
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2024-01-28 00:07:12 +0530

    Add files via upload

---
+++ PriorityQueue/base.java
@@ -0,0 +1,19 @@
+package PriorityQueue;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class base {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq =new PriorityQueue<>(Comparator.reverseOrder());
+
+        pq.add(3);
+        pq.add(1);
+        pq.add(0);
+        pq.add(4);
+
+        while (!pq.isEmpty()){
+            System.out.println(pq.peek());
+            pq.remove();
+        }
+    }
+}
