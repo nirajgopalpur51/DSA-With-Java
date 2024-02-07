2def73177e0311dcc35d75ba6bd7daa4391a2137
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2024-02-07 21:54:13 +0530

    Add files via upload

---
+++ Dynamic Programming/fibbonacci.java
@@ -0,0 +1,20 @@
+package DP;
+
+public class fibbonacci {
+    public static int fib(int n,int dp[]){
+        if(n==1 || n==0){
+            return n;
+        }
+        if(dp[n] != 0){
+            return dp[n];
+        }
+        dp[n] = fib(n-1,dp) + fib(n-2,dp);
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int n=8;
+        int dp[] =new int[n+1];
+        System.out.println(fib(n,dp));
+    }
+}
