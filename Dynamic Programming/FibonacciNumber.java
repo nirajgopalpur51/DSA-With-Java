3b8baef79050666c926dd7653c1478b5f1fc7129
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2024-02-09 23:17:16 +0530

    Add files via upload

---
+++ Dynamic Programming/FibonacciNumber.java
@@ -0,0 +1,17 @@
+package DP;
+
+public class FibonacciNumber {
+    public static int fib(int n) {
+        if (n <= 1) return n;
+        int dp[]=new int[n+1];
+        dp[1]=1;
+        for(int i=2; i<=n; i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fib(8));
+    }
+}
