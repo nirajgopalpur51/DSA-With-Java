096cf32a70fbd46532444099667e28fead767aa5
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2024-03-09 08:25:45 +0530

    Add files via upload

---
+++ Arrays/MinimumCommonValue.java
@@ -0,0 +1,24 @@
+package Array;
+import java.util.*;
+public class MinimumCommonValue {
+    public static int getCommon(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        int result=-1;
+        for(int num : nums1){
+            set.add(num);
+        }
+        for(int num: nums2){
+            if(set.contains(num)){
+                result =num;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+       int [] nums1 = {1, 2, 3};
+        int [] nums2 = {2, 4};
+        System.out.println(getCommon(nums1,nums2));
+    }
+}
