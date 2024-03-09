2732354cf908fa9558ed73b583f953075dd95e55
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2024-03-09 08:01:45 +0530

    Add files via upload

---
+++ Arrays/FirstMissingPositive.java
@@ -0,0 +1,33 @@
+package Array;
+
+public class FirstMissingPositive {
+    public static int firstMissingPositive(int[] nums) {
+        int i = 0;
+        while(i < nums.length) {
+            int correct = nums[i] - 1;
+            if(nums[i] <= nums.length && nums[i] > 0 && nums[i] != nums[correct]) {
+
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            }
+            else {
+                i++;
+            }
+        }
+
+        int count = 0;
+        for(int j = 0; j < nums.length; j++) {
+
+            if(nums[j] != j + 1) {
+                return j + 1;
+            }
+        }
+        return nums.length + 1;
+    }
+
+    public static void main(String[] args) {
+        int [] nums={3,4,-1,1};
+        System.out.println(firstMissingPositive(nums));
+    }
+}
