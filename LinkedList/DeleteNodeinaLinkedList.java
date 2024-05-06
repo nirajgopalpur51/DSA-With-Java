716da854d83e427019af472d70df34ec1c21d761
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2024-05-06 22:16:32 +0530

    Add files via upload

---
+++ LinkedList/DeleteNodeinaLinkedList.java
@@ -0,0 +1,8 @@
+package LinkedList;
+
+public class DeleteNodeinaLinkedList {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
