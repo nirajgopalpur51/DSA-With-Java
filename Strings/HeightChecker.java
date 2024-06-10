96caac6fbf973ee7a5d1c301005aa02bab36a0ef
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2024-06-10 11:58:31 +0530

    Add files via upload

---
+++ Strings/HeightChecker.java
@@ -0,0 +1,30 @@
+package String;
+import java.util.*;
+
+public class HeightChecker {
+    public static int heightChecker(int[] heights) {
+
+        int count =0;
+
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for(int e : heights){
+            arr.add(e);
+        }
+
+        Collections.sort(arr);
+
+        for(int i=0; i<heights.length; i++){
+            if(heights[i] != arr.get(i)){
+                count++;
+            }
+        }
+        return count;
+
+    }
+
+    public static void main(String[] args) {
+       int [] heights = {1,1,4,2,1,3};
+        System.out.println(heightChecker(heights));
+    }
+}
