aa20463c31e90265a622ca9eb3e49f6c06dcc11e
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2024-06-15 10:57:13 +0530

    Add files via upload

---
+++ Arrays/FindPeakElement.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class FindPeakElement {
+    public int findPeakElement(int[] nums) {
+        int maxSoFar = Integer.MIN_VALUE;
+        int index = 0;
+
+        int i=0;
+        for(int n : nums){
+            if(n > maxSoFar){
+                maxSoFar = n;
+                index = i;
+            }
+            i++;
+        }
+        return index;
+    }
+}
