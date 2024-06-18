7fd031202676727e5245f2a1695a5d05df464e3c
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2024-06-18 13:39:29 +0530

    Add files via upload

---
+++ Arrays/RearrangeArrayElementsbySign.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class RearrangeArrayElementsbySign {
+    public int[] rearrangeArray(int[] nums) {
+
+        int n = nums.length;
+        int[] res = new int[n];
+        int positive=0, negative=1;
+
+        for(int i=0; i<n; i++){
+            if(nums[i]>0){
+                res[positive]=nums[i];
+                positive += 2;
+            }
+            else{
+                res[negative]=nums[i];
+                negative += 2;
+            }
+        }
+        return res;
+
+    }
+}
