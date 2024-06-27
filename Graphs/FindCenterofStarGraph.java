80bb66222ac31ee31978b1b3547ce51496726361
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2024-06-27 23:13:39 +0530

    Add files via upload

---
+++ Graphs/FindCenterofStarGraph.java
@@ -0,0 +1,12 @@
+package Graphs;
+
+public class FindCenterofStarGraph {
+    public int findCenter(int[][] edges) {
+        int pair1[] = edges[0];
+        int pair2[] = edges[1];
+
+        if(pair1[0] == pair2[0] || pair1[0] == pair2[1]) return pair1[0];
+
+        return pair1[1];
+    }
+}
