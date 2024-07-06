ee83efd57543a9bc733edde891939f76abb647a6
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2024-07-06 22:52:35 +0530

    Add files via upload

---
+++ Numberof1Bits.java
@@ -0,0 +1,10 @@
+public class Numberof1Bits {
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            count += (n & 1);
+            n = n >> 1;
+        }
+        return count;
+    }
+}
