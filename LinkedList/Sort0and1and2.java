7080255f102adf9236d5566fc36794a1c204b7da
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2024-07-14 10:50:48 +0530

    Add files via upload

---
+++ LinkedList/Sort0and1and2.java
@@ -0,0 +1,47 @@
+package LinkedList;
+
+public class Sort0and1and2 {
+    static Node segregate(Node head) {
+        // add your code here
+        int zero = 0;
+        int one = 0;
+        int two = 0;
+        Node current = head;
+        Node temp = head;
+
+
+        while(current!=null){
+            if(current.data == 0){
+                zero++;
+                current = current.next;
+            }
+            else if(current.data == 1){
+                one++;
+                current = current.next;
+            }
+            else{
+                two++;
+                current = current.next;
+            }
+        }
+
+        while(temp != null){
+            if(zero>0){
+                temp.data = 0;
+                zero--;
+                temp=temp.next;
+            }
+            else if(one>0){
+                temp.data = 1;
+                one--;
+                temp=temp.next;
+            }
+            else if(two>0){
+                temp.data = 2;
+                two--;
+                temp=temp.next;
+            }
+        }
+        return head;
+    }
+}
